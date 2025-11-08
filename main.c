25741985f3796bcf3b1717fe81909a02e40f660e
nour-hussin <n0ur0eldiin@gmail.com>
2025-11-08 18:06:22 +0200

    refactor: organize code structure

--- main.c
+++ main.c
@@ -2,155 +2,25 @@
 
 int main()
 {
-    int row = RowBias, col = ColBias, out_of_text = 0, out_of_program = 0 , mode, display_option, text_color=0, back_color=7;
-    char char_arr[MaxRows][MaxLineSize] = {{'\0'}}, filename[100], ctrl;
-do{
-    mode = print_main_menu();
-    switch(mode)
-    {
+//------------------------------------------ Variables --------------------------------------------------------
+    int row = RowBias, col = ColBias;
+    int out_of_text = 0, out_of_program = 0 ;
+    int mode, display_option, color_option;
+    int text_color = 0, back_color = 7;
+    char char_arr[MaxRows][MaxLineSize] = {{'\0'}};
+    char filename[100]={'\0'};
+    char ctrl;
 
-//---------------------------------------- First Mode ---------------------------------------------------------------\\
-
+    do{
+        mode = print_main_menu();
+        switch(mode){
+//---------------------------------------- First Mode ---------------------------------------------------------------
         case 0: // NEW
-            out_of_text = 0;
-            print_char_window(char_arr, RowBias, ColBias, back_color, text_color);
-            gotoxy(col,row);
 
-        do{
-        ctrl = getch();
-        switch(ctrl)
-        {
-            case -32: // Extended key
-            case 0:
-                ctrl = getch();
-                switch(ctrl)
-                {
-                    case 73: //PageUp
-                        row = RowBias;
-                        col = ColBias;
-                        gotoxy(col,row);
-                        break;
-
-                    case 81: //PageDown
-                        row = RowBias + last_row(char_arr);
-                        col = ColBias + last_col(char_arr[row-RowBias]);
-                        gotoxy(col,row);
-                        break;
-
-                    case 72: // Up Arrow
-                        row --;
-                        if (row < RowBias) row = RowBias;
-
-                        if (col > ColBias + last_col(char_arr[row-RowBias]))
-                            col = ColBias + last_col(char_arr[row-RowBias]);
-
-                        gotoxy(col,row);
-                        break;
-
-                    case 80: // Down Arrow
-                        row ++;
-                        if (row > RowBias + last_row(char_arr))
-                            row = RowBias + last_row(char_arr);
-
-                        if (col > ColBias + last_col(char_arr[row-RowBias]))
-                            col = ColBias + last_col(char_arr[row-RowBias]);
-
-                        gotoxy(col,row);
-                        break;
-
-                    case 77: // Right Arrow
-                        col ++;
-                        if (col > ColBias + strlen(char_arr[row-RowBias]))
-                        {
-                            col = ColBias;
-                            row ++;
-                            if (row > RowBias + last_row(char_arr))
-                            {
-                                row = RowBias + last_row(char_arr);
-                                col = ColBias + strlen(char_arr[row-RowBias]);
-                            }
-
-                        }
-                        gotoxy(col,row);
-                        break;
-
-                    case 75: // Left Arrow
-                        col --;
-                        if (col<ColBias)
-                        {
-                            row --;
-                            if (row < RowBias)
-                            {
-                                row = RowBias;
-                                col = ColBias;
-                            }
-                            else
-                                col = ColBias + strlen(char_arr[row-RowBias]);
-                        }
-                        gotoxy(col,row);
-                        break;
-
-                    case 71: // Home Key
-                        col = ColBias;
-                        gotoxy(col,row);
-                        break;
-
-                    case 79: // End Key
-                        col = ColBias + last_col(char_arr[row-RowBias]);
-                        gotoxy(col,row);
-                        break;
-
-                    case 83: // Delete Key
-                        delete_char(char_arr[row-RowBias], col-ColBias);
-                        gotoxy(ColBias,row); printf("%s", char_arr[row-RowBias]);for(int i = 0; i<(MaxLineSize-strlen(char_arr[row-RowBias])); i++) {printf(" ");} gotoxy(col,row);
-                        break;
-                    case 82: // Insert Key
-                        insert_char(char_arr[row-RowBias],getch(),col-ColBias);
-                        gotoxy(ColBias,row); printf("%s", char_arr[row-RowBias]);for(int i = 0; i<(MaxLineSize-strlen(char_arr[row-RowBias])); i++) {printf(" ");} gotoxy(col,row);
-                        break;
-                }
-                break;
-
-            case  8: // Backspace Key
-                    delete_char(char_arr[row-RowBias], col - ColBias -1);
-                    col --;
-                    if (col<ColBias) col = ColBias;
-                    gotoxy(ColBias,row); printf("%s", char_arr[row-RowBias]); for(int i = 0; i<(MaxLineSize-strlen(char_arr[row-RowBias])); i++) {printf(" ");} gotoxy(col,row);
-                    break;
-
-            case 13: // Enter key
-                row ++;
-                col = ColBias;
-                if (row > RowBias + MaxRows) row = RowBias + MaxRows;
-                gotoxy(col,row);
-                break;
-
-            case 27: // Escape key or Tab
-            case 9:
-                out_of_text = 1;
-                textattr(0x07);
-                break;
-
-            default:
-                if (col >= ColBias + MaxLineSize)
-                {
-                    col = ColBias;
-                    row ++;
-                }
-                if (row >= RowBias + MaxRows)
-                    row = RowBias + MaxRows;
-                char_arr[row-RowBias][col-ColBias] = ctrl;
-                gotoxy(col,row); printf("%c",ctrl);
-                col ++;
-
-        }
-
-    } while(out_of_text == 0);
-
+            run_edit_mode(char_arr, &back_color, &text_color);
             break;
-
-//---------------------------------------- Second Mode ---------------------------------------------------------------\\
-
+            
+//---------------------------------------- Second Mode --------------------------------------------------------------
         case 1: // DISPLAY
             textattr(0x60);
             printf("\n\nWanna Display:\n1.Current Text\n2.Open File\n");
@@ -188,8 +58,7 @@ do{
             textattr(0x07);
             break;
 
-//---------------------------------------- Third Mode ----------------------------------------------------------------\\
-
+//---------------------------------------- Third Mode ----------------------------------------------------------------
         case 2: //SAVE
             printf("\n\nEnter file name to save (e.g. output.txt): ");
             scanf("%99s", filename);
@@ -203,8 +72,7 @@ do{
             getch();
             break;
 
-//---------------------------------------- Fourth Mode ---------------------------------------------------------------\\
-
+//---------------------------------------- Fourth Mode ---------------------------------------------------------------
         case 3: // COLOR
             printf("\n\n\n");
             for(int i = 0; i < 8; i++)
@@ -221,14 +89,12 @@ do{
             getch();
             break;
 
-//---------------------------------------- Fifth Mode ----------------------------------------------------------------\\
-
+//---------------------------------------- Fifth Mode ----------------------------------------------------------------
         case 4: // EXIT
             clrscr();
             out_of_program = 1;
             break;
-    }
-} while(out_of_program == 0);
-
-    return 0;
+        }
+    } while(out_of_program == 0);
+return 0;
 }
--- text_editor.h
+++ text_editor.h
@@ -1,13 +1,79 @@
+#ifndef TEXT_EDITOR_H
+#define TEXT_EDITOR_H
+
 #include <stdio.h>
 #include <stdlib.h>
-#include "D:\Nour_Hussin\Career_Electronics\ITI 9 Months\C\Day2\MyLibrary.h"
 #include <conio.h>
 #include <string.h>
+#include <windows.h>
 
 #define MaxLineSize   90
 #define MaxRows       20
 #define RowBias       4
 #define ColBias       0
+
+// Cursor and console handling
+void gotoxy(int x, int y);
+void clrscr(void);
+void textattr(int ForgC);
+void SetColor(int ForgC);
+
+// Text operations
+void delete_char(char *char_array, int index);
+void insert_char(char *char_array, char ch, int index);
+int last_row(char arr[MaxRows][MaxLineSize]);
+int last_col(char arr[MaxLineSize]);
+
+// Printing
+void print_char_array(char arr[MaxRows][MaxLineSize], int row_bias, int col_bias);
+void print_char_window(char arr[MaxRows][MaxLineSize], int row_bias, int col_bias, int back_color, int text_color);
+
+int print_main_menu(void);
+void run_edit_mode(char char_arr[MaxRows][MaxLineSize], int *back_color, int *text_color);
+
+#endif
+
+
+
+
+
+void gotoxy(int x, int y)
+{
+    COORD coordinate;
+    coordinate.X = x;
+    coordinate.Y = y;
+    SetConsoleCursorPosition(GetStdHandle(STD_OUTPUT_HANDLE), coordinate);
+}
+
+void clrscr()
+{
+    system("cls");
+}
+
+void textattr(int ForgC)
+{
+    //system("COLOR ForgC"); //system("COLOR FC");
+    HANDLE hStdOut = GetStdHandle(STD_OUTPUT_HANDLE);
+    SetConsoleTextAttribute(hStdOut, ForgC);
+}
+
+void SetColor(int ForgC)
+{
+     WORD wColor;
+     //This handle is needed to get the current background attribute
+
+     HANDLE hStdOut = GetStdHandle(STD_OUTPUT_HANDLE);
+     CONSOLE_SCREEN_BUFFER_INFO csbi;
+     //csbi is used for wAttributes word
+
+     if(GetConsoleScreenBufferInfo(hStdOut, &csbi))
+     {
+          //To mask out all but the background attribute, and to add the color
+          wColor = (csbi.wAttributes & 0xF0) + (ForgC & 0x0F);
+          SetConsoleTextAttribute(hStdOut, wColor);
+     }
+     return;
+}
 
 void delete_char(char *char_array, int index)
 {
@@ -34,10 +100,13 @@ void insert_char(char *char_array, char ch, int index)
 
 int last_row(char arr[MaxRows][MaxLineSize])
 {
+    int last = -1;
     for (int i = 0; i < MaxRows; i++)
-        if(arr[i][0] == '\0')
-            return i-1;
-        return MaxRows;
+    {
+        if (arr[i][0] != '\0')
+            last = i;
+    }
+    return (last == -1) ? 0 : last;
 }
 
 int last_col(char arr[MaxLineSize])
@@ -65,8 +134,6 @@ void print_char_array(char arr[MaxRows][MaxLineSize], int row_bias, int col_bias
 
 void print_char_window(char arr[MaxRows][MaxLineSize],int row_bias, int col_bias, int back_color, int text_color)
 {
-    int current_row, current_col;
-
     if(back_color > 7) back_color = 0; // Convert to valid color
     if(text_color > 7) text_color = 7; // Convert to valid color
     textattr(((back_color & 0xF) << 4) | (text_color & 0xF));
@@ -124,4 +191,139 @@ int print_main_menu()
     } while(out == 0);
     textattr(0x07);
     return current_option;
+}
+
+void run_edit_mode(char char_arr[MaxRows][MaxLineSize], int *back_color, int *text_color)
+{
+    int row = RowBias, col = ColBias;
+    int out_of_text = 0;
+    char ctrl;
+
+    print_char_window(char_arr, RowBias, ColBias, *back_color, *text_color);
+    gotoxy(col, row);
+
+    do {
+        ctrl = getch();
+        switch (ctrl)
+        {
+            case -32: // extended key
+            case 0:
+                ctrl = getch();
+                switch (ctrl)
+                {
+                    case 73: // PageUp
+                        row = RowBias;
+                        col = ColBias;
+                        break;
+
+                    case 81: // PageDown
+                        row = RowBias + last_row(char_arr);
+                        col = ColBias + last_col(char_arr[row - RowBias]);
+                        break;
+
+                    case 72: // Up
+                        row--;
+                        if (row < RowBias) row = RowBias;
+                        if (col > ColBias + last_col(char_arr[row - RowBias]))
+                            col = ColBias + last_col(char_arr[row - RowBias]);
+                        break;
+
+                    case 80: // Down
+                        row++;
+                        if (row > RowBias + last_row(char_arr))
+                            row = RowBias + last_row(char_arr);
+                        if (col > ColBias + last_col(char_arr[row - RowBias]))
+                            col = ColBias + last_col(char_arr[row - RowBias]);
+                        break;
+
+                    case 77: // Right
+                        col++;
+                        if (col > ColBias + strlen(char_arr[row - RowBias]))
+                        {
+                            col = ColBias;
+                            row++;
+                            if (row > RowBias + last_row(char_arr))
+                            {
+                                row = RowBias + last_row(char_arr);
+                                col = ColBias + strlen(char_arr[row - RowBias]);
+                            }
+                        }
+                        break;
+
+                    case 75: // Left
+                        col--;
+                        if (col < ColBias)
+                        {
+                            row--;
+                            if (row < RowBias)
+                            {
+                                row = RowBias;
+                                col = ColBias;
+                            }
+                            else
+                                col = ColBias + strlen(char_arr[row - RowBias]);
+                        }
+                        break;
+
+                    case 71: // Home
+                        col = ColBias;
+                        break;
+
+                    case 79: // End
+                        col = ColBias + last_col(char_arr[row - RowBias]);
+                        break;
+
+                    case 83: // Delete
+                        delete_char(char_arr[row - RowBias], col - ColBias);
+                        gotoxy(ColBias, row);
+                        printf("%-*s", MaxLineSize, char_arr[row - RowBias]);
+                        break;
+
+                    case 82: // Insert
+                        insert_char(char_arr[row - RowBias], getch(), col - ColBias);
+                        gotoxy(ColBias, row);
+                        printf("%-*s", MaxLineSize, char_arr[row - RowBias]);
+                        break;
+                }
+                break;
+
+            case 8: // Backspace
+                delete_char(char_arr[row - RowBias], col - ColBias - 1);
+                col--;
+                if (col < ColBias) col = ColBias;
+                gotoxy(ColBias, row);
+                printf("%-*s", MaxLineSize, char_arr[row - RowBias]);
+                break;
+
+            case 13: // Enter
+                row++;
+                col = ColBias;
+                if (row > RowBias + MaxRows - 1)
+                    row = RowBias + MaxRows - 1;
+                break;
+
+            case 27: // ESC
+            case 9:  // TAB
+                out_of_text = 1;
+                textattr(0x07);
+                break;
+
+            default: // Normal characters
+                if (col >= ColBias + MaxLineSize)
+                {
+                    col = ColBias;
+                    row++;
+                }
+                if (row >= RowBias + MaxRows)
+                    row = RowBias + MaxRows - 1;
+
+                char_arr[row - RowBias][col - ColBias] = ctrl;
+                gotoxy(col, row);
+                putchar(ctrl);
+                col++;
+        }
+
+        gotoxy(col, row);
+
+    } while (out_of_text == 0);
 }
